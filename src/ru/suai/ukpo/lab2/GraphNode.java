4259ce2369cbc2727bc0986623f79416961e3407
Leonif Volkov <cornsnake@protonmail.com>
2020-06-26 02:45:31 +0300

    Correct package structure

--- src/ru/suai/ukpo/lab2/GraphNode.java
+++
@@ -1,5 +0,0 @@
-package ru.suai.ukpo.lab2;
-
-public interface GraphNode {
-    String getId();
-}
--- src/ru/suai/ukpo/lab2/Scorer.java
+++
@@ -1,5 +0,0 @@
-package ru.suai.ukpo.lab2;
-
-public interface Scorer<T extends GraphNode> {
-    double computeCost(T from, T to);
-}
